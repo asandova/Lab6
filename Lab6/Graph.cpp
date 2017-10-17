9f09729e54b940e8f30dbdac0024589d0c4f528c
August Sandoval <31152904+asandova@users.noreply.github.com>
2017-10-17 14:05:12 -0600

    inital commit

---
+++ Lab6/Graph.cpp
@@ -0,0 +1,242 @@
+/**
+*
+*CS372: Lab5
+*File: Graph.cpp
+*Author: August B. Sandoval
+*Purpose: Defines the Graph class in Graph.h
+*
+**/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <fstream>
+
+#include <cstring>
+
+#include "Node.h"
+#include "Graph.h"
+
+using namespace std;
+
+
+Graph::Graph(const string & file, bool dir){
+    Directed = dir;
+    m_nodes = vector<Node>();
+    m_adjList = vector<list<Node> >();
+    scan(file);
+}
+
+Graph::Graph(const string& file){
+    Directed = false;
+    m_nodes = vector<Node>();
+    m_adjList = vector<list<Node> >();
+    scan(file);
+}
+//Insert a edge ( a ,b ) to m_adjList
+void Graph::addEdge ( const Node & a , const Node & b ) {
+    //the addEdge will add node b to node a's adj list if
+        //the node is not already in the list
+            //and inserts the nodes in alphabetical order
+    if( getAdjNodes(a).empty() ){
+        getAdjNodes(a).push_back(b);
+    }
+    else if(!NodeExistAdj( b, a.id() ) ){
+        list<Node> adjList = getAdjNodes(a);
+        for(list<Node>::iterator itr = adjList.begin(); itr != adjList.end(); ++itr){
+            if(*itr > b){
+                //--itr;
+                m_adjList[a.id()].insert(itr,b);
+                return;
+            }
+        }
+            m_adjList[a.id()].push_back(b);
+    }
+    if(Directed == false){
+        if( getAdjNodes(b).empty() ){
+            getAdjNodes(b).push_back(a);
+        }
+        else if(!NodeExistAdj( a, b.id() ) ){
+            list<Node> adjList = getAdjNodes(b);
+            for(list<Node>::iterator itr = adjList.begin(); itr != adjList.end(); ++itr){
+                if(*itr > a){
+                    //--itr;
+                    m_adjList[b.id()].insert(itr,a);
+                    return;
+                }
+            }
+                m_adjList[b.id()].push_back(a);
+        }
+    }
+}
+
+//Insert a node a to m_nodes
+void Graph::addNode ( const Node & a ) {
+    ///add the node to the Graph's node vector
+    ///if the node does not already exist
+    //checking is the node is already in the vector
+    if( !NodeExist(a.name() ) ){
+        //m_nodes.reserve( m_nodes.size() + 1 );
+        //m_nodes [ a.id( ) ] = a ;
+        m_nodes.push_back(a);
+        list<Node>adj;
+        m_adjList.push_back(adj);
+    }
+}
+
+
+bool Graph::NodeExistAdj(const Node& a, size_t id)const{
+    ///checks if node a is in the adj list for the node with the ID value of id
+    const list<Node> Adjlist = getAdjNodes( getNode(id) );
+    for(list<Node>::const_iterator itr = Adjlist.begin(); itr != Adjlist.end(); ++itr ){
+        if(a == *itr)
+            return true;
+    }
+    return false;
+}
+
+bool Graph::NodeExist(const string& name)const{
+    Node curr;
+    for(size_t i = 0; i < m_nodes.size(); i++){
+        curr = getNode(i);
+        if(curr.name() == name){
+            return true;
+        }
+    }
+    return false;
+}
+
+size_t Graph::findID(const string & name)const{
+    Node curr;
+    for(size_t i = 0; i < m_nodes.size(); i++){
+        curr = getNode(i);
+        if(curr.name() == name){
+            return curr.id();
+        }
+    }
+    return -1;
+}
+
+// Return node with id equal to i
+Node & Graph::getNode ( size_t i ){
+    return m_nodes[ i ] ;
+}
+const Node & Graph::getNode ( size_t i )const{
+    return m_nodes[ i ] ;
+}
+
+// Return reference of the adjacency list of node a
+list <Node> & Graph::getAdjNodes ( const Node & a ){
+    return m_adjList[ a.id ( ) ] ;
+}
+
+// Return constant reference to adjacency list of node a
+const list <Node> & Graph::getAdjNodes ( const Node & a ) const{
+    return m_adjList [ a.id ( ) ] ;
+}
+
+bool Graph::allExplored(size_t id)const{
+    list<Node> L = getAdjNodes( getNode( id ) );
+    for(list<Node>::const_iterator itr = L.begin(); itr != L.end(); ++itr){
+        if(itr->getPreTime() == 0){
+            return true;
+        }
+    }
+    return false;
+}
+
+// Return the total number of nodes i n the graph
+size_t Graph::num_nodes ( ) const {
+    return m_nodes.size( ) ;
+}
+
+//only splits strings into two on tab character
+vector<string> split(const string& a){
+    vector<string> names;
+    for(size_t i = 0; i < a.size()-1; i++ ){
+        if(a[i] == '\t'){
+            names.push_back( a.substr(0, i-1)   ) ;
+            names.push_back( a.substr(i+1, a.size() ) );
+            break;
+        }
+    }
+    return names;
+}
+
+// Create a graph from a tab−separated text edge list file
+// to adjacency lists
+void Graph::scan ( const string & file ){
+
+    //char* line;
+    string fline;
+    int id = 0;
+    Node N1, N2;
+    ifstream iFile;
+    iFile.open( file.c_str(), ifstream::in );
+    if(  iFile.is_open()  ){
+        while( !iFile.eof() ){
+                getline(iFile, fline);
+                //string L(line);
+                //vector<string> names = split(fline);
+                vector<string>names(2);
+                names[0] = fline.c_str()[0];
+                names[1] = fline.c_str()[2];
+
+                if(!NodeExist( names[0] ) ){
+                        Node tmp1( names[0],id++);
+                        N1 = tmp1;
+                        addNode(N1);
+                }else{
+                    N1 = getNode( findID( names[0] ) ) ;
+                }
+                if(!NodeExist( names[1] ) ){
+                        Node tmp2( names[1] ,id++);
+                        N2 = tmp2;
+                        addNode( N2 ) ;
+                }else{
+                        N2 = getNode( findID( names[1] ) ) ;
+                }
+                addEdge(N1, N2);
+        }//end of while
+        iFile.close();
+    }else{
+        cout << "File: " << file << " does not exist" << endl;
+    }//end of else
+}//end of scan
+
+// Save a graph from adjacency lists to a tab−separated
+// text edge list file
+void Graph::save( const string & file ){
+    //NOTE: the method assumes the file does not exist and will overwrite
+    // if the file does exist
+        cout << "in save" << endl;
+        ofstream OFile;
+        OFile.open(file.c_str(), ofstream::out);
+        for(size_t i =0; i < m_nodes.size(); i++){
+            const list<Node> neighbors = getAdjNodes( getNode(i) );
+            for(list<Node>::const_iterator itr = neighbors.begin(); itr!= neighbors.end(); ++itr){
+                    OFile << getNode(i).name() << "\t" << itr->name() << "\n";
+            }
+        }
+        OFile.close();
+}
+
+ostream& operator<<(ostream & out, const Graph & g){
+    out << "Nodes in the graph: " << endl ;
+    for ( unsigned i =0; i <g.num_nodes ( ) ; i ++) {
+        out << g.getNode( i ).name ( ) << " ," ;
+    }
+    out << endl ;
+    out << "Adjacency list of the graph : " << endl ;
+    for ( unsigned i =0; i <g.num_nodes( ) ; i ++) {
+        out << "Node " << g.getNode(i).name( ) << " : ";
+        const list <Node> neighbors = g.getAdjNodes ( g.getNode(i) ) ;
+            for( list<Node>::const_iterator itr = neighbors.begin( ) ;
+                itr!= neighbors.end ( ) ; ++itr ) {
+                out << itr->name( ) << " (" << itr->getPreTime() << ", " << itr->getPostTime() << "), " ;
+            }
+        out << endl;
+    }
+    return out;
+}
---
+++ Lab6/main.cpp
@@ -0,0 +1,152 @@
+/**
+*
+*CS372: Lab5
+*File: main.cpp
+*Author: August B. Sandoval
+*Purpose: The main file for Lab5
+*
+**/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+
+#include <cstdlib>
+#include <cstdio>
+#include <ctype.h>
+
+#include "Node.h"
+#include "Graph.h"
+
+using namespace std;
+
+void Explore(Graph & G, Node & C);
+
+int GTime;
+Node nulNode("",0);
+
+void DFS(Graph & G){ ///linear time DFS algorithm
+    cout << "in DFS" << endl;
+    for(size_t i = 0; i < G.num_nodes(); i ++){ ///finds unexplored node
+        if( G.getNode(i).getPreTime() == 0  ){ ///checks if Node with ID i is unexplored
+            G.getNode(i).setPreTime(GTime++); ///sets pretime of unexplored node
+            list<Node> adj = G.getAdjNodes( G.getNode(i) );/// gets the adjacency list
+            for(list<Node>::iterator itr = adj.begin(); itr != adj.end(); ++itr){ ///looks for next unexplored node connected to current node
+                if( itr->getPreTime() == 0 ){///check if node in list is unexplored
+                        itr->setPreTime(GTime++);///sets pretime of node
+                        Explore(G,*itr); ///find next unexplored node
+                        itr->setPostTime(GTime++);///sets post time of node
+                        cout << *itr << endl << endl;
+                }
+            }//end of for
+        }//end of if
+    }//end of for
+}
+
+void DFSRecursive(Graph & G){ /// Recursive DFS algorithm
+    Node starting = nulNode;
+    for(size_t i = 0;i < G.num_nodes(); i++){
+        if(starting < G.getNode(i) && G.getNode(i).getPreTime() == 0){
+            starting = G.getNode(i);
+        }
+    }
+    if(starting == nulNode)
+        return;
+    else{
+        starting.setPreTime(GTime++);
+        Explore(G,starting);
+        starting.setPostTime(GTime++);
+        DFSRecursive(G);
+    }
+}
+
+void DFSItative(Graph & G){ /// Iterative DFS algorithm
+    ///uses stacks
+
+    stack<Node> dfs;
+    if(G.num_nodes() == 0){
+        return;
+    }
+    G.getNode(0).setPreTime(GTime++);
+    dfs.push( G.getNode( 0 ) );
+    while(!dfs.empty()){
+        list<Node> adj = G.getAdjNodes( dfs.top() );
+        for(list<Node>::iterator itr = adj.begin(); itr != adj.end(); ++itr){
+            if(itr->getPreTime() == 0){
+                itr->setPreTime(GTime++);
+                dfs.push(*itr);
+                continue;
+            }
+        }
+        dfs.top().setPostTime(GTime++);
+        dfs.pop();
+    }
+
+}
+
+void Explore(Graph & G, Node & C){
+    //check if the node has been visited
+    if(C.getPreTime() != 0) //base case
+        return;
+
+    //set post time
+    C.setPreTime(GTime++);
+     //look for next node
+     list<Node> links = G.getAdjNodes( C );
+     for(list<Node>::iterator itr = links.begin(); itr != links.end() ; ++itr ){
+            Explore(G,*itr);
+     }
+    //set post time
+   C.setPostTime(GTime++);
+}
+
+void testall(){
+
+    Graph Tgraph1("Graph1.txt",1);
+    cout << Tgraph1 << endl;
+    //Graph Tgraph2("Graph2.txt",1);
+    //Graph Tgraph3("Graph3.txt",1);
+    //Tgraph1.save("Graph1out.txt");
+    //Tgraph2.save("Graph2out.txt");
+    //Tgraph3.save("Graph3out.txt");
+
+
+    ///test linear
+    GTime = 1;
+    DFS(Tgraph1);
+    cout << GTime << endl;
+    cout << Tgraph1 << endl;
+    ///test recursive
+    //GTime = 1;
+    //DFSRecursive(Tgraph2);
+    //cout << Tgraph2 << endl;
+    ///test iterative
+    //GTime = 1;
+    //DFSItative(Tgraph3);
+    //cout << Tgraph3 << endl;
+
+
+}
+
+int main(){
+
+    testall();
+
+    return 0;
+}
+
+/** R
+
+require("igraph")
+
+links <- read.table("file.txt", header = F, quote = "", stringsAsFactors = F)
+names(links) <- c("from", "to")
+#links <- data.frame( from=c("a", "a", "c", "d", "e" ),
+#                                         to = c("d", "b", "b", "c", "a"))
+net <- graph_from_data_frame(d=links, directed=T)
+plot(net, vertex.size=30, vertex.label.cex=2)
+
+net <- graph_from_data_frame(d=links, directed=F)
+plot(net, vertex.size=30,vertex.label.cex=2)
+
+*/
